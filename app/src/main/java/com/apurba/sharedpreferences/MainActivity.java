33893a4a2c46638938f79c1f31ba7668fca0331d
apurba <apurba10024@gmail.com>
2019-08-27 22:45:26 +0600

    Updated code with Resources

--- app/src/main/java/com/apurba/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/apurba/sharedpreferences/MainActivity.java
@@ -1,12 +1,15 @@
 package com.apurba.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        updateUiFromPreference();
     }
 
+
+    private void updateUiFromPreference(){
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        TextView tvGender = findViewById(R.id.tv_gender);
+        String text = (sharedPreferences.getBoolean(getString(R.string.preference_gender_key)
+                , getResources().getBoolean(R.bool.preference_gander_default)))?
+                getString(R.string.preference_gender_summary_on) :
+                getString(R.string.preference_gender_summary_off);
+        tvGender.setText(text);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
